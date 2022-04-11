f225d759a841b8e8c0a53a90f841378b7849c915
Quyen Pham <hoangquyen174@gmail.com>
2022-04-11 15:00:55 -0500

    upload models

---
+++ models/user.js
@@ -0,0 +1,136 @@
+"use strict";
+
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const {
+    NotFoundError,
+    BadRequestError,
+    UnauthorizedError,
+  } = require("../expressError");
+const { devNull } = require("os");
+
+
+class User {
+    static async authenticate(username, password) {
+      const result = await db.query(
+            `SELECT username,
+                    password,
+                    first_name AS "firstName",
+                    last_name AS "lastName",
+                    email,
+                    is_admin AS "isAdmin"
+             FROM users
+             WHERE username = $1`,
+          [username],
+      );
+  
+      const user = result.rows[0];
+  
+      if (user) {
+        const isValid = await bcrypt.compare(password, user.password);
+        if (isValid === true) {
+          delete user.password;
+          return user;
+        }
+      }
+  
+      throw new UnauthorizedError("Invalid username/password");
+    }
+  
+    static async register(
+        { username, password, firstName, lastName, email, isAdmin }) {
+      const duplicateCheck = await db.query(
+            `SELECT username
+             FROM users
+             WHERE username = $1`,
+          [username],
+      );
+  
+      if (duplicateCheck.rows[0]) {
+        throw new BadRequestError(`Duplicate username: ${username}`);
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+  
+      const result = await db.query(
+            `INSERT INTO users
+             (username,
+              password,
+              first_name,
+              last_name,
+              email,
+              is_admin)
+             VALUES ($1, $2, $3, $4, $5, $6)
+             RETURNING username, first_name AS "firstName", last_name AS "lastName", email, is_admin AS "isAdmin"`,
+          [
+            username,
+            hashedPassword,
+            firstName,
+            lastName,
+            email,
+            isAdmin,
+          ],
+      );
+  
+      const user = result.rows[0];
+  
+      return user;
+    }
+  
+    static async findAll() {
+      const result = await db.query(
+            `SELECT username,
+                    first_name AS "firstName",
+                    last_name AS "lastName",
+                    email,
+                    is_admin AS "isAdmin"
+             FROM users
+             ORDER BY username`,
+      );
+  
+      return result.rows;
+    }
+  
+    static async get(username) {
+      const userRes = await db.query(
+            `SELECT username,
+                    first_name AS "firstName",
+                    last_name AS "lastName",
+                    email,
+                    is_admin AS "isAdmin"
+             FROM users
+             WHERE username = $1`,
+          [username],
+      );
+  
+      const user = userRes.rows[0];
+  
+      if (!user) throw new NotFoundError(`No user: ${username}`);
+  
+      const userApplicationsRes = await db.query(
+            `SELECT a.job_id
+             FROM applications AS a
+             WHERE a.username = $1`, [username]);
+  
+      user.applications = userApplicationsRes.rows.map(a => a.job_id);
+      return user;
+    }
+
+    /** Delete given user from database; returns undefined. */
+  
+    static async remove(username) {
+      let result = await db.query(
+            `DELETE
+             FROM users
+             WHERE username = $1
+             RETURNING username`,
+          [username],
+      );
+      const user = result.rows[0];
+  
+      if (!user) throw new NotFoundError(`No user: ${username}`);
+    }
+  }
+  
+  
+  module.exports = User;
